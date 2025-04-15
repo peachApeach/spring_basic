64c029f74bccc22d290f14f4eeb31188a04b725b
peachApeach <yh_01@daum.net>
2025-04-15 22:04:50 +0900

    CHORE: change spring

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,13 +9,19 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+// @Configuration과 @Bean을 사용하면 스프링 컨테이너에 저장 됨 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         // return new FixDiscountPolicy();
         return new RateDiscountPolicy();
@@ -23,11 +29,13 @@ public class AppConfig {
 
     // 중복 코드 제거
     // MemberService 구현체 설정 -> 생성자를 통해서 구현체 결정
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
     // Order
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,12 +4,19 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
         //MemberService memberService = new MemberServiceImpl();
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+        //AppConfig appConfig = new AppConfig();
+        //MemberService memberService = appConfig.memberService();
+        //spring은 모두 ApplicationContext로 시작 (컨테이너)
+        //아래 구문으로 인하여 AppConfig에 있는 정보를 토대로 스프링 사용가능
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        //메서드 이름과 클래스(반환 타입) 명시
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Member member = new Member(Grade.VIP, "memberA", 1L);
         memberService.join(member);
