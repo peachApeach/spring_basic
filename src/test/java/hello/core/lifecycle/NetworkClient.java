44d49da74964a73c508a86a646705ffed5640e74
peachApeach <yh_01@daum.net>
2025-05-20 14:52:26 +0900

    ADD: spring lifecycle

---
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,35 @@
+package hello.core.lifecycle;
+
+public class NetworkClient {
+    // 가상 네트워크 클라이언트
+
+    // 접속해야 할 url
+    private String url;
+
+    // 디폴트 생성자
+    public NetworkClient() {
+        System.out.println("생성자 호출 url = " + url);
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    // 외부 입력
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    // 서비스 시작 시 호출하는 메서드
+    public void connect() {
+        System.out.println("connect " + url);
+    }
+
+    // connect 후 콜 할 수 있다고 가정
+    public void call(String message) {
+        System.out.println("call : " + url + " message = " + message);
+    }
+
+    // 서비스 종료 시 호출
+    public void disconnect() {
+        System.out.println("close " + url);
+    }
+}
