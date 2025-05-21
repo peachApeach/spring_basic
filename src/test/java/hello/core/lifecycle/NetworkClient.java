dc86e2ad9912c4a59642526e03a81e024be80522
peachApeach <yh_01@daum.net>
2025-05-21 12:26:42 +0900

    ADD: InitializingBean, DisposableBean

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,6 +1,9 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
     // 가상 네트워크 클라이언트
 
     // 접속해야 할 url
@@ -9,8 +12,8 @@ public class NetworkClient {
     // 디폴트 생성자
     public NetworkClient() {
         System.out.println("생성자 호출 url = " + url);
-        connect();
-        call("초기화 연결 메시지");
+//        connect();
+//        call("초기화 연결 메시지");
     }
 
     // 외부 입력
@@ -31,5 +34,20 @@ public class NetworkClient {
     // 서비스 종료 시 호출
     public void disconnect() {
         System.out.println("close " + url);
+    }
+
+    // 의존 관계 주입 후 호출
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        connect();
+        call("초기화 연결 메시지");
+        System.out.println("afterPropertiesSet");
+    }
+
+    // 소멸 전 콜백
+    @Override
+    public void destroy() throws Exception {
+        disconnect();
+        System.out.println("destroy");
     }
 }
